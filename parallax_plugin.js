bc51a3286ed97ef5b890506a08d5fa6bfeef8d45
Keshav Tiwari <keshav786tiwari@gmail.com>
2018-07-11 17:57:14 +0530

    Update parallax_plugin.js

--- parallax_plugin.js
+++ parallax_plugin.js
@@ -5,11 +5,13 @@
 		if(speed > 1){ speed=speed*-1;}
 		if(speed > 0 && speed < 1){speed = 1 - speed}
 		var x = $(this);
+		var height = x.position().top;
+		if(x.css('position') == 'static'){ x.css('position', 'absolute');}
 		var scroll = $(document).scrollTop();
-		x.css('top',  scroll*speed+'px');
+		x.css('top',  height+scroll*speed+'px');
 		$(document).scroll(function(){
 			scroll = $(document).scrollTop();
-			x.css('top',  scroll*speed+'px');
+			x.css('top',  height+scroll*speed+'px');
 		});
 	}
 }
